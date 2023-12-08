c50e2f914cd794a2889be27226fba3ea63071c30
airon <airon.gomes@hotmail.com>
2023-12-07 21:54:22 -0300

    add comments

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,3 +1,8 @@
+/*
+* As an extra activity, I added that, when saving, it is possible to keep the number of times each Goal was achieved. When Loading, it is possible to retrieve this information and thus maintain the score data obtained.
+* The GetScorePoints() abstrack was added to Goal and each child of this class implements this function according to its characteristics.
+*/
+
 using System;
 
 class Program
